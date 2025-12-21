c1ed8dfabfbdfdb54d0d06361072fb408dda24fb
mrifala29 <mrifala29@gmail.com>
2025-12-21 23:27:25 +0700

    Update minor in S3 upload feature

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.modules.categories.router import router as category_router
 from app.modules.models.router import router as model_router
 from app.modules.items.router import router as item_router
+from app.modules.images.router import router as image_router
 
 app = FastAPI(title="Inventory Intelligence API")
 
